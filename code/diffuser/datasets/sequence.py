f2bf03cbdcaef3fa71fc584fdeef3980e375f873
Manasi Sharma <manasis@iliad5.stanford.edu>
2023-01-05 15:29:42 -0800

    update

--- code/diffuser/datasets/sequence.py
+++ code/diffuser/datasets/sequence.py
@@ -106,7 +106,6 @@ class SequenceDataset(torch.utils.data.Dataset):
             batch = RewardBatch(trajectories, conditions, returns)
         else:
             batch = Batch(trajectories, conditions)
-
         return batch
 
 class CondSequenceDataset(torch.utils.data.Dataset):
