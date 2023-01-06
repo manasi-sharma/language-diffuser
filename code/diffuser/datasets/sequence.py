ea66db2d530076150ed82d6a1ab0302d0495003c
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-01-06 01:08:33 -0800

    update

--- code/diffuser/datasets/sequence.py
+++ code/diffuser/datasets/sequence.py
@@ -76,11 +76,15 @@ class SequenceDataset(torch.utils.data.Dataset):
         fields = ReplayBuffer(max_n_episodes, max_path_length) #, termination_penalty)
         for i, batch in enumerate(calvin_dataloader):
             episode = {}
-            perceptual_emb = model.perceptual_encoder(batch['rgb_obs'], batch["depth_obs"], batch["robot_obs"]).squeeze().detach().numpy() #torch.Size([32, 32, 3, 200, 200]) --> torch.Size([32, 32, 72])
-            latent_goal = model.language_goal(batch['lang']).detach().numpy() #torch.Size([32, 384]) --> torch.Size([32, 32])
+            if train_flag:
+                batch_obj = batch
+            else:
+                batch_obj = batch['lang']
+            perceptual_emb = model.perceptual_encoder(batch_obj['rgb_obs'], batch_obj["depth_obs"], batch_obj["robot_obs"]).squeeze().detach().numpy() #torch.Size([32, 32, 3, 200, 200]) --> torch.Size([32, 32, 72])
+            latent_goal = model.language_goal(batch_obj['lang']).detach().numpy() #torch.Size([32, 384]) --> torch.Size([32, 32])
             len_hor = len(perceptual_emb)
             latent_goal = np.tile(latent_goal, (len_hor, 1))
-            action_emb = batch['actions'].squeeze().detach().numpy()
+            action_emb = batch_obj['actions'].squeeze().detach().numpy()
             episode['observations'] = perceptual_emb
             episode['actions'] = action_emb
             episode['language'] = latent_goal
