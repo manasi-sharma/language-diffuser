89af93f962b813710bdc34c2be4d41cc68f408a9
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-01-13 18:39:37 -0800

    update

--- code/diffuser/models/temporal.py
+++ code/diffuser/models/temporal.py
@@ -212,6 +212,7 @@ class TemporalUnet(nn.Module):
         if self.returns_condition:
             assert returns is not None
             returns_embed = self.returns_mlp(returns)
+            import pdb;pdb.set_trace()
             if use_dropout:
                 mask = self.mask_dist.sample(sample_shape=(returns_embed.size(0), 1)).to(returns_embed.device)
                 returns_embed = mask*returns_embed
