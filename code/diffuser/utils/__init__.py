10d76fd09a5c647d4e9042b2c905c567c072e5cb
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-01-19 21:45:56 -0800

    update

--- code/diffuser/utils/__init__.py
+++ code/diffuser/utils/__init__.py
@@ -3,6 +3,6 @@ from .training import *
 from .progress import *
 from .setup import *
 from .config import *
-from .rendering import *
+#from .rendering import *
 from .arrays import *
 from .colab import *
