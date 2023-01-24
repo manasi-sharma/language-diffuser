1b21b8dfed5a1bc075aa31d810426bdcea0e8862
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-01-24 15:55:56 -0800

    update

--- code/scripts/calvin_agent/datasets/utils/episode_utils.py
+++ code/scripts/calvin_agent/datasets/utils/episode_utils.py
@@ -50,17 +50,17 @@ def process_state(
         seq_state_obs = seq_state_obs_unnormalized
 
     # slice the specified parts of the proprioception state
-    print("\n\n\nOGGGG Seq state: ", seq_state_obs.shape)
+    #print("\n\n\nOGGGG Seq state: ", seq_state_obs.shape)
     state_obs_sliced = []
     for slice_ids in proprio_state.keep_indices:
         seq_state_obs_ = seq_state_obs[:, slice(*slice_ids)]
-        print("\nintermediate state: ", seq_state_obs_.shape)
-        print("\nintermediate slice_ids: ", slice_ids)
+        #print("\nintermediate state: ", seq_state_obs_.shape)
+        #print("\nintermediate slice_ids: ", slice_ids)
         state_obs_sliced.append(seq_state_obs_)
-    state_obs_sliced.append()
+
     seq_state_obs = torch.cat(state_obs_sliced, dim=1)
-    print("\n\n\nlen: ", len(state_obs_sliced))
-    print("\n\n\nUpdateddd state: ", seq_state_obs.shape)
+    #print("\n\n\nlen: ", len(state_obs_sliced))
+    #print("\n\n\nUpdateddd state: ", seq_state_obs.shape)
     return {"robot_obs": seq_state_obs}
 
 
