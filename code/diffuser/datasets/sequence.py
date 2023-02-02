b3117ea36fd7644ff2e884477392d36e49cc694f
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-02-01 23:01:00 -0800

    update

--- code/diffuser/datasets/sequence.py
+++ code/diffuser/datasets/sequence.py
@@ -64,13 +64,13 @@ class SequenceDataset(torch.utils.data.Dataset):
             else:
                 batch_obj = batch['lang']
             
-            batch_obj["robot_obs"] = batch_obj["robot_obs"].to(torch.device("cuda:0"))
-            batch_obj["lang"] = batch_obj["lang"].to(torch.device("cuda:0"))
+            #batch_obj["robot_obs"] = batch_obj["robot_obs"].to(torch.device("cuda:0"))
+            #batch_obj["lang"] = batch_obj["lang"].to(torch.device("cuda:0"))
 
-            perceptual_emb = model.perceptual_encoder.proprio_encoder(batch_obj["robot_obs"]).squeeze(0).detach().numpy() # torch.Size([1, 32, 32]) --> torch.Size([32, 32])
-            latent_goal = model.language_goal(batch_obj['lang']).detach().numpy() #torch.Size([32, 384]) --> torch.Size([32, 32])
+            perceptual_emb = model.perceptual_encoder.proprio_encoder(batch_obj["robot_obs"]).squeeze(0).numpy() # torch.Size([1, 32, 32]) --> torch.Size([32, 32])
+            latent_goal = model.language_goal(batch_obj['lang']).numpy() #torch.Size([32, 384]) --> torch.Size([32, 32])
             len_hor = len(perceptual_emb)
-            action_emb = batch_obj['actions'].squeeze().detach().numpy()
+            action_emb = batch_obj['actions'].squeeze().numpy()
             episode['observations'] = perceptual_emb
             episode['actions'] = action_emb
             episode['language'] = latent_goal
