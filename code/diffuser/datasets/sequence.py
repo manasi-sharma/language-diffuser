69e458e15fa5f5d05cd6c5dfeb75f2c2e2df3dc3
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-02-02 00:12:09 -0800

    update

--- code/diffuser/datasets/sequence.py
+++ code/diffuser/datasets/sequence.py
@@ -66,7 +66,6 @@ class SequenceDataset(torch.utils.data.Dataset):
             
             batch_obj["robot_obs"] = batch_obj["robot_obs"].to(torch.device("cuda:0"))
             batch_obj["lang"] = batch_obj["lang"].to(torch.device("cuda:0"))
-            import pdb;pdb.set_trace()
 
             perceptual_emb = model.perceptual_encoder.proprio_encoder(batch_obj["robot_obs"]).squeeze(0).cpu().numpy() # torch.Size([1, 32, 32]) --> torch.Size([32, 32])
             latent_goal = model.language_goal(batch_obj['lang']).detach().cpu().numpy() #torch.Size([32, 384]) --> torch.Size([32, 32])
