76a57b7c12f4226788393f3f8a405a6862e65346
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-02-08 13:24:04 -0800

    update

--- code/diffuser/utils/training.py
+++ code/diffuser/utils/training.py
@@ -170,7 +170,7 @@ class Trainer(object):
         if self.save_checkpoints:
             savepath = os.path.join(savepath, f'state_{self.step}.pt')
         else:
-            savepath = os.path.join(savepath, 'state.pt') #'state_debug_5000_100_200.pt')
+            savepath = os.path.join(savepath, 'state_debug_1e6_1e5_200.pt') #'state_debug_5000_100_200.pt')
         torch.save(data, savepath)
         #logger.print(f'[ utils/training ] Saved model to {savepath}')
 
