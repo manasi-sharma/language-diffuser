8335ad244f1a4176a4b3dcc1587fb17c07746b0d
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-02-18 19:37:30 -0800

    update

--- code/diffuser/datasets/sequence.py
+++ code/diffuser/datasets/sequence.py
@@ -145,15 +145,15 @@ class SequenceDataset(torch.utils.data.Dataset):
             returns = (discounts * rewards).sum()
             returns = np.array([returns/self.returns_scale], dtype=np.float32)"""
             fields_language = self.fields.language[:, start:end, :]
+            t1 = time()
             fields_language = np.unique(fields_language, axis=1)
             if fields_language.shape[1] != 1:
                 print("\n\nError!\n\n")
                 import pdb;pdb.set_trace()
             returns = fields_language[path_ind, 0]
-            #import pdb;pdb.set_trace()
-            t1 = time()
-            batch = RewardBatch(trajectories, conditions, returns)
             print("\n\n\nLOSS TIME: ", time()-t1, "\n\n\n")
+            #import pdb;pdb.set_trace()
+            batch = RewardBatch(trajectories, conditions, returns)
         else:
             batch = Batch(trajectories, conditions)
         
