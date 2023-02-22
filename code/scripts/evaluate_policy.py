9f243fdd9bc84aab3e9446e48f1f8fed3d87027a
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-02-21 23:35:20 -0800

    update

--- code/scripts/evaluate_policy.py
+++ code/scripts/evaluate_policy.py
@@ -62,7 +62,7 @@ def get_log_dir(log_dir):
 class CustomModel:
     def __init__(self, cfg):
         #state_dict = torch.load(f'/iliad/u/manasis/language-diffuser/code/logs/checkpoint/state.pt',
-        state_dict = torch.load(f'/iliad/u/manasis/language-diffuser/code/logs/checkpoint/debug_1e5_1000_200.pt',
+        state_dict = torch.load(f'/iliad/u/manasis/language-diffuser/code/logs/checkpoint/new_langdiffuser_1e5_1000_1_bsize_32.pt',
                                 map_location=Config.device)
 
         dataset_config = utils.Config(
@@ -449,7 +449,7 @@ def wrap_main(config_name):
         #args = parser.parse_args()
         args = Args()
         args.dataset_path = '/iliad/u/manasis/language-diffuser/code/calvin_debug_dataset'
-        args.train_folder = '/iliad/u/manasis/language-diffuser/code/outputs/2023-02-19/01-57-28/'
+        args.train_folder = '/iliad/u/manasis/language-diffuser/code/outputs/2023-02-21/11-48-42/'
         args.checkpoints = None
         args.checkpoint = None
         args.last_k_checkpoints =  None
