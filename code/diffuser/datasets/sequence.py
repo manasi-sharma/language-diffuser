b9549126b2aa517626f8c0a4f4a4902550676603
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-02-22 18:47:47 -0800

    update

--- code/diffuser/datasets/sequence.py
+++ code/diffuser/datasets/sequence.py
@@ -60,6 +60,13 @@ class SequenceDataset(torch.utils.data.Dataset):
 
         """Creating embeddings initialization"""
         fields = ReplayBuffer(max_n_episodes, max_path_length) #, termination_penalty)
+        t1 = time()
+        for i, batch in enumerate(calvin_dataloader):
+            if i == 10:
+                break
+        print("\n\n\nLOSS NONE TIME: ", time()-t1, "\n\n\n")
+        import pdb;pdb.set_trace()
+
         #import pdb;pdb.set_trace()
         t1= time()
         for i, batch in enumerate(calvin_dataloader):
