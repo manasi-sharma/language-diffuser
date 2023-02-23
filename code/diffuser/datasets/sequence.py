8d7739b8749c2558f8366996a1553ca7a770d4ee
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-02-22 17:58:18 -0800

    update

--- code/diffuser/datasets/sequence.py
+++ code/diffuser/datasets/sequence.py
@@ -69,16 +69,15 @@ class SequenceDataset(torch.utils.data.Dataset):
             else:
                 batch_obj = batch['lang']
             
-            
-            #batch_obj["robot_obs"] = batch_obj["robot_obs"].to(torch.device("cuda"))
-            #batch_obj["lang"] = batch_obj["lang"].to(torch.device("cuda"))
+            batch_obj["robot_obs"] = batch_obj["robot_obs"].to(torch.device("cuda"))
+            batch_obj["lang"] = batch_obj["lang"].to(torch.device("cuda"))
 
             t1= time()
             perceptual_emb = model.perceptual_encoder.proprio_encoder(batch_obj["robot_obs"]).squeeze(0).cpu().numpy() # torch.Size([1, 32, 32]) --> torch.Size([32, 32])
             latent_goal = model.language_goal(batch_obj['lang']).detach().cpu().numpy() #torch.Size([32, 384]) --> torch.Size([32, 32])
             print("\n\n\nLOSS TIME: ", time()-t1, "\n\n\n")
             import pdb;pdb.set_trace()
-            
+
             len_hor = len(perceptual_emb)
             action_emb = batch_obj['actions'].squeeze().numpy()
             episode['observations'] = perceptual_emb
