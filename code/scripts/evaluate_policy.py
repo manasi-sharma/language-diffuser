91550258e6d6d8743ef208a661a438a017b0e512
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-02-26 23:33:49 -0800

    update

--- code/scripts/evaluate_policy.py
+++ code/scripts/evaluate_policy.py
@@ -62,7 +62,7 @@ def get_log_dir(log_dir):
 class CustomModel:
     def __init__(self, cfg):
         #state_dict = torch.load(f'/iliad/u/manasis/language-diffuser/code/logs/checkpoint/state.pt',
-        state_dict = torch.load(f'/iliad/u/manasis/language-diffuser/code/logs/checkpoint/langdiffuser_1e6_10000_200_bsize_8_debug.pt',
+        state_dict = torch.load(f'/iliad/u/manasis/language-diffuser/code/logs/checkpoint/langdiffuser_1e5_1000_200_bsize_8_debug_newdataloader.pt',
                                 map_location=Config.device)
 
         dataset_config = utils.Config(
